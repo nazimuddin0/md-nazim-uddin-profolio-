623a9b83bdfd4c9f4ddf5165ff8fb50f8f6a96c2
MD Nazim Uddin <145536117+nazimuddin0@users.noreply.github.com>
2025-04-16 14:26:37 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('nav');
+    
+    // Toggle navigation
+    hamburger.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+    
+    // Close menu on click
+    document.querySelectorAll('nav a').forEach(link => {
+        link.addEventListener('click', () => {
+            nav.classList.remove('active');
+        });
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+    const typingElement = document.getElementById('typing');
+    const words = ["Analyst", "Researcher", "Engineer", "Pentester"];
+    let wordIndex = 0;
+    let letterIndex = 0;
+    let currentWord = '';
+    let currentLetters = '';
+    let isDeleting = false;
+    function type() {
+        if (isDeleting) {
+            currentLetters = currentWord.substring(0, letterIndex - 1);
+            letterIndex--;
+        } else {
+            currentLetters = currentWord.substring(0, letterIndex + 1);
+            letterIndex++;
+        }
+
+        typingElement.innerHTML = currentLetters;
+
+        let typeSpeed = 200;
+        if (isDeleting) {
+            typeSpeed /= 2;
+        }
+
+        if (!isDeleting && letterIndex === currentWord.length) {
+            typeSpeed = 2000;
+            isDeleting = true;
+        } else if (isDeleting && letterIndex === 0) {
+            isDeleting = false;
+            wordIndex++;
+            if (wordIndex === words.length) {
+                wordIndex = 0;
+            }
+            currentWord = words[wordIndex];
+            typeSpeed = 500;
+        }
+
+        setTimeout(type, typeSpeed);
+    }
+    currentWord = words[wordIndex];
+    type();
+});
